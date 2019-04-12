e4d3d05ceed4368a2be0a4b2c9937dd75a7195d5
Akira Sawada <aklaswad@gmail.com>
2019-04-12 18:22:31 +0900

    Revert "Attempt to make D:OD fork safe"

--- lib/Data/ObjectDriver/BaseObject.pm
+++ lib/Data/ObjectDriver/BaseObject.pm
@@ -8,10 +8,6 @@ our $HasWeaken;
 eval q{ use Scalar::Util qw(weaken) }; ## no critic
 $HasWeaken = !$@;
 
-our $HasAtFork;
-eval q{ use POSIX::AtFork }; ## no critic
-$HasAtFork = !$@;
-
 use Carp ();
 
 use Class::Trigger qw( pre_save post_save post_load pre_search
@@ -669,14 +665,6 @@ sub has_partitions {
             $obj->partition_id($partition_id);
             $orig_obj->partition_id($partition_id);
         }
-    });
-}
-
-# At fork, forget about parent's current transaction status.
-if ( $HasAtFork ) {
-    POSIX::AtFork->add_to_child(sub {
-        @WorkingDrivers = ();
-        $TransactionLevel = 0;
     });
 }
 
